627b9106128aa4de0dbd46fffc7819e912b42adb
Yury Paraschenko <y_paraschenko@wargaming.net>
2017-12-27 17:37:49 +0300

    added makeForwards

--- core/receiver/announce/processAnnounce.go
+++ core/receiver/announce/processAnnounce.go
@@ -2,6 +2,9 @@ package announce
 
 import (
 	"github.com/vvampirius/retracker/bittorrent/tracker"
+	"github.com/vvampirius/retracker/bittorrent/common"
+	"fmt"
+	CoreCommon "github.com/vvampirius/retracker/core/common"
 )
 
 func (self *Announce) ProcessAnnounce(remoteAddr, infoHash, peerID, port, uploaded, downloaded, left, ip, numwant,
@@ -17,6 +20,7 @@ func (self *Announce) ProcessAnnounce(remoteAddr, infoHash, peerID, port, upload
 			if request.Event != `stopped` {
 				self.Storage.Update(*request)
 				response.Peers = self.Storage.GetPeers(request.InfoHash)
+				response.Peers = append(response.Peers, self.makeForwards(*request)...)
 			} else {
 				self.Storage.Delete(*request)
 				//TODO: make another response ?
@@ -28,4 +32,26 @@ func (self *Announce) ProcessAnnounce(remoteAddr, infoHash, peerID, port, upload
 		}
 
 		return nil
+}
+
+func (self *Announce) makeForwards(request tracker.Request) []common.Peer {
+	peers := make([]common.Peer, 0)
+	forwardsCount := len(self.Config.Forwards)
+	if forwardsCount > 0 {
+		ch := make(chan []common.Peer, forwardsCount)
+		for _, v := range self.Config.Forwards {
+			self.makeForward(v, request, ch)
+		}
+		for i := 0; i < forwardsCount; i++ {
+			fmt.Println(<-ch)
+		}
+		fmt.Println("makeForwards exit")
+	}
+	return peers
+}
+
+func (self *Announce) makeForward(forward CoreCommon.Forward, request tracker.Request, ch chan<- []common.Peer) {
+	peers := make([]common.Peer, 0)
+	fmt.Println(forward, request)
+	ch <- peers
 }
